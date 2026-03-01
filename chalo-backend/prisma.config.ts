ee3aa9f1f606c8a8ba901ef2641f6017a314ed06
Verma0830 <156420621+Verma0830@users.noreply.github.com>
2026-03-01 21:08:01 +0530

    Update prisma.config.ts

--- chalo-backend/prisma.config.ts
+++ chalo-backend/prisma.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'prisma/config'
-import * as dotenv from 'dotenv'
-
-dotenv.config()
+import 'dotenv/config'
 
 export default defineConfig({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
+  migrate: {
+    url: process.env.DATABASE_URL!,
   },
 })
